ad77e72dd337eb1d477cd8fbf6c7b4c64af9852b
Matthew Collins <matthew.collins@defra.gov.uk>
2020-06-17 09:18:22 +0100

    Fix issues raised in review

--- scripts/anonymise-data.js
+++ scripts/anonymise-data.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const parse = require('csv-parse')
 const fs = require('fs')
 
@@ -23,24 +25,24 @@ const processArgs = (args) => {
 const config = processArgs(process.argv)
 let processed = 0
 const SbiReplacementList = ['106599008']
-let fakeSBICounter = 107000000
+let fakeSbiCounter = 107000000
 let fakeAgreementCounter = 11111
 let fakeParcelId = 1111222
-const fakeSbiList = []
-const getFakeSbi = (SBI) => {
-  const result = fakeSbiList[SBI]
-  if (result) {
-    return result
+const fakeSbiList = {}
+const getFakeIdentifiers = (SBI) => {
+  const existingFakeIdentifiers = fakeSbiList[SBI]
+  if (existingFakeIdentifiers) {
+    return existingFakeIdentifiers
   } else {
-    let sbiToUse = fakeSBICounter
+    let sbiToUse = fakeSbiCounter
     if (SbiReplacementList.length > 0) {
       sbiToUse = SbiReplacementList.pop()
     }
-    const retval = [sbiToUse, fakeAgreementCounter]
-    fakeSBICounter++
+    const newFakeIdentifiers = [sbiToUse, fakeAgreementCounter]
+    fakeSbiCounter++
     fakeAgreementCounter++
-    fakeSbiList[SBI] = retval
-    return retval
+    fakeSbiList[SBI] = newFakeIdentifiers
+    return newFakeIdentifiers
   }
 }
 
@@ -59,9 +61,9 @@ parser.on('readable', async () => {
     }
     // SBI,AGREEMENT_CODE,AGREEMENT_DESC,SCHEME_OPTION,SCHEME_OPTION_DESC,DURATION,AGREEMENT_START_YR,AGREEMENT_END_YR,AGREEMENT_START_DT,AGREEMENT_END_DT,PARCEL_ID,PARCEL,HECTARES
     if (processed > 1) {
-      const [fakeSbi, fakeAgreement] = getFakeSbi(record[0])
-      record[0] = fakeSbi
-      record[1] = record[1].replace(/\d*(\/.*)*/, `${fakeAgreement}$1`)
+      const [fakeSbiId, fakeAgreementId] = getFakeIdentifiers(record[0])
+      record[0] = fakeSbiId
+      record[1] = record[1].replace(/\d*(\/.*)*/, `${fakeAgreementId}$1`)
       record[10] = fakeParcelId
       record[11] = 'NN22224444'
       for (const i of [1, 2, 3, 4, 8, 9, 10, 11]) {
@@ -72,7 +74,5 @@ parser.on('readable', async () => {
     console.log(record.join(','))
   }
 })
-// parser.on('end', () => {
-// })
 
 inStream.pipe(parser)
